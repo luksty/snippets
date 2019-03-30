dd04c0b310ce12538b03835c67ba149bfde6b1db
LukaszStyrylski <lukasz.styrylski@givt.com>
2019-03-30 14:38:50 +0100

    ML_snippets.py

---
+++ ML_snippets.py
@@ -0,0 +1,12 @@
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt  
+
+m = 100
+X = 6 * np.random.rand(m, 1) - 3
+y = 0.5 * X**2 + X + 2 + np.random.randn(m, 1)
+plt.scatter(X, y)  
+plt.show()
+
+from sklearn.model_selection import train_test_split  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)  
